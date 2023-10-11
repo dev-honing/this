82e9a4f6e58cc6b3397b5e666fdb2ace12f1dad0
Tech1997 <dev.honing@gmail.com>
2023-10-11 10:56:10 +0900

    콘솔 확인 - 거푸집처럼 틀을 만든 것

--- newfile.js
+++ newfile.js
@@ -13,3 +13,10 @@ let list = ["이민구", "윤준현"];
 for(let i=0; i < list.length; i++ ){
   studentArr[i] = new student(list[i], i+1);
 }
+console.log(studentArr); // 콘솔로 찍힌 내용을 확인하기
+// [
+//   student { name: '이민구', number: 1 },
+//   student { name: '윤준현', number: 2 }
+// ]
+
+// 거푸집의 원리와 같다 : 틀을 만든 것!
