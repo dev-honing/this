1bbf1b9f4000d2bd95cd8ffcde640d3ab6f02667
Tech1997 <dev.honing@gmail.com>
2023-10-11 10:48:28 +0900

    this를 이용해 객체를 뽑아낸 것, 기존의 리터럴과 다른 점

--- newfile.js
+++ newfile.js
@@ -4,3 +4,9 @@ function student(name, number){
 }
 let test = new student("변호녕", "0")
 console.log(test); // 콘솔에 찍어보기 : 객체가 생성됨!
+// -> 이 작업은 객체를 뽑아낸 것!
+
+let studentList = {
+  name : "hello",
+  number : 1,
+} // -> 지금까지는 이렇게 객체를 리터럴로 만들어왔다.
