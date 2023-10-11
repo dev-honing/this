2431b20a86f7d336fc7ea039c1ac011c212df6d7
Tech1997 <dev.honing@gmail.com>
2023-10-11 10:50:19 +0900

    빈 배열 studentArr 변수 선언

--- newfile.js
+++ newfile.js
@@ -3,10 +3,6 @@ function student(name, number){
   this.number = number;
 }
 let test = new student("변호녕", "0")
-console.log(test); // 콘솔에 찍어보기 : 객체가 생성됨!
-// -> 이 작업은 객체를 뽑아낸 것!
 
-let studentList = {
-  name : "hello",
-  number : 1,
-} // -> 지금까지는 이렇게 객체를 리터럴로 만들어왔다.
+// 빈 배열을 변수 선언
+let studentArr = [];
