056f8292b4c5543b8a33a773e84fcedc6f007381
Tech1997 <dev.honing@gmail.com>
2023-10-11 12:03:43 +0900

    버튼 생성

--- arrow-1.js
+++ arrow-1.js
@@ -2,4 +2,6 @@ function outerOne(a){
   const innerOne = b => console.log(b);
   innerOne(a);
 }
-console.log(outerOne("hello"));
+// 오늘 핵심 정리
+// 화살표 함수를 보면 겁먹지 말고 계속 사용해보며 연습하기
+// this 키워드는 일단 사용하지 않기
