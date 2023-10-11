39ec2ff56fe40dee75e031ecb059b4a483b3dd24
Tech1997 <dev.honing@gmail.com>
2023-10-11 10:52:49 +0900

    배열 list를 변수 선언하고 반복문과 new 기능을 이용한 로직 작성

--- newfile.js
+++ newfile.js
@@ -6,3 +6,10 @@ let test = new student("변호녕", "0")
 
 // 빈 배열을 변수 선언
 let studentArr = [];
+
+// 배열 변수 선언
+let list = ["이민구", "윤준현"];
+
+for(let i=0; i < list.length; i++ ){
+  studentArr[i] = new student(list[i], i+1);
+}
